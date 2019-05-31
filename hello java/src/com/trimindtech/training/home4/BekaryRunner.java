6e2d131c271b00ec055996e1084d8c9a55ce23f8
battulajhansi <jhansibtech123@gmail.com>
2019-05-31 10:31:02 +0530

    java_training

---
+++ hello java/src/com/trimindtech/training/home4/BekaryRunner.java
@@ -0,0 +1,12 @@
+package com.trimindtech.training.home4;
+
+public class BekaryRunner {
+    public static void main(String[] args) {
+
+
+        Bekary masala = new Bekary("rm25", "rm50", "rm75");
+        //masala.printthedetails;
+        System.out.println(masala.getOnekg());
+
+    }
+}
